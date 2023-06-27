f8122f3c3f3fc9b49315d203e0eb9a4a77b017a2
Antonio <novoselskiantonio@gmail.com>
2023-06-28 00:36:22 +0200

    Fixed View Model

---
+++ BurgerApp/BurgerApp.ViewModels/BurgerViewModels/BurgerListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BurgerApp.ViewModels.BurgerViewModels
+{
+    public class BurgerListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
